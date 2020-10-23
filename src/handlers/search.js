e125a1ca97b81dcf1602900bcc9794ad9a7b3c01
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-10-23 16:17:15 +0530

    feat: search for a module by name

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -1,5 +1,35 @@
-const searchModule = () => {
-  // TODO
+const { prompt, registerPrompt, Separator } = require('inquirer');
+const fuzzy = require('fuzzy');
+const nuxtModules = require('@nuxt/modules');
+
+registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+const fetchModuleInfo = require("./helpers");
+
+const searchModules = (answers, input) => {
+  input = input || '';
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const fuzzyResult = fuzzy.filter(input, nuxtModules.map(({ name }) => name));
+      const results = fuzzyResult.map(({ original }) => original);
+
+      // results.splice(5, 0, new Separator());
+      results.push(new Separator());
+      resolve(results);
+    }, Math.random() * 500);
+  });
+}
+
+const searchModule = async () => {
+  const { module } = await prompt({
+      type: 'autocomplete',
+      name: 'module',
+      message: 'Search a module',
+      source: searchModules,
+    });
+
+  // Display the information associated with the respective Nuxt.js module
+  fetchModuleInfo(module);
 };
 
 module.exports = searchModule;
